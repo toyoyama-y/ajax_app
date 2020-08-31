9237a9532010e48153f170377c4aa78f9fb78f77
toyoyama-y <star.pratinum.7@gmail.com>
2020-08-31 20:31:48 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,9 @@ class PostsController < ApplicationController
     @posts = Post.all.order(id: "DESC")
   end
 
-
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
-  end
+    post = Post.create(content: params[:content], checked: false)
+    render json:{ post: post }
 
   def checked
     post = Post.find(params[:id])
